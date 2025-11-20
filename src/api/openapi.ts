727703fed4cbc25bbd7a85d5fd7f88ab19243728
김태우 <taewoo080401@gmail.com>
2025-11-20 16:00:48 +0900

    feat: firebase

--- src/api/openapi.ts
+++ src/api/openapi.ts
@@ -2,10 +2,10 @@ import axios from "axios"
 
 export const OpenApi = {
   async fetchData(pageNo: number) {
-    return await axios.get(`http://apis.data.go.kr/6260000/FestivalService/getFestivalKr?ServiceKey=${import.meta.env.VITE_API_KEY}&pageNo=${pageNo}&numOfRows=20&resultType=json`);
+    return await axios.get(`http://apis.data.go.kr/6260000/FestivalService/getFestivalKr?ServiceKey=${import.meta.env.VITE_SERVICE_KEY}&pageNo=${pageNo}&numOfRows=20&resultType=json`);
   },
-  
+
   async fetchOne(id: number) {
-    return await axios.get(`http://apis.data.go.kr/6260000/FestivalService/getFestivalKr?ServiceKey=${import.meta.env.VITE_API_KEY}&pageNo=1&UC_SEQ=${id}&numOfRows=1&resultType=json`);
+    return await axios.get(`http://apis.data.go.kr/6260000/FestivalService/getFestivalKr?ServiceKey=${import.meta.env.VITE_SERVICE_KEY}&pageNo=1&UC_SEQ=${id}&numOfRows=1&resultType=json`);
   }
 }
